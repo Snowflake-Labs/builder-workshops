c36f89c3ea0dcd0f32beb01736f121aece3333bd
Gilberto Hernandez <gilberto.hernandez@snowflake.com>
2024-05-08 22:11:32 -0500

    Create setup.sql

---
+++ auto-grader/setup.sql
@@ -0,0 +1,52 @@
+use role accountadmin;
+
+create or replace api integration dora_api_integration 
+api_provider = aws_api_gateway 
+api_aws_role_arn = 'arn:aws:iam::321463406630:role/snowflakeLearnerAssumedRole' 
+enabled = true 
+api_allowed_prefixes = ('https://awy6hshxy4.execute-api.us-west-2.amazonaws.com/dev/edu_dora');
+
+create database util_db;
+use database util_db;
+use schema public;
+
+create or replace external function util_db.public.grader(        
+ step varchar     
+ , passed boolean     
+ , actual integer     
+ , expected integer    
+ , description varchar) 
+ returns variant 
+ api_integration = dora_api_integration 
+ context_headers = (current_timestamp,current_account, current_statement, current_account_name) 
+ as 'https://awy6hshxy4.execute-api.us-west-2.amazonaws.com/dev/edu_dora/grader'  
+;  
+
+select grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
+ 'AUTO_GRADER_IS_WORKING' as step
+ ,(select 123) as actual
+ ,123 as expected
+ ,'The Snowflake auto-grader has been successfully set up in your account!' as description
+);
+
+create or replace external function util_db.public.greeting(
+      email varchar
+    , firstname varchar
+    , middlename varchar
+    , lastname varchar)
+returns variant
+api_integration = dora_api_integration
+context_headers = (current_timestamp, current_account, current_statement, current_account_name) 
+as 'https://awy6hshxy4.execute-api.us-west-2.amazonaws.com/dev/edu_dora/greeting'
+; 
+
+-- Replace <email> with your email address
+-- Replace <first name> <middle name> <last name> with your names
+-- Be sure to follow the rules your session leader presents
+select util_db.public.greeting(<email>, <first name>, <middle name>, <last name>);
+
+-- Example: With middle name
+-- select util_db.public.greeting('my@email.com', 'Snowflake', 'The', 'Bear');
+
+-- Example: With no middle name
+-- select util_db.public.greeting('my@email.com', 'Snowflake', '', 'Bear');
