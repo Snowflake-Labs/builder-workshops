9eb221e73dd5c6987877d61b8b27185ca30b80ee
rsafdar44 <rida.safdar@snowflake.com>
2024-12-11 11:04:41 -0800

    Update llm-ops.sql
    removing steps 3 and 5 due to user errors. Now only 6 steps required for successful completion

--- gen-ai/llm-ops.sql
+++ gen-ai/llm-ops.sql
@@ -19,30 +19,12 @@ select util_db.public.grader(step, (actual = expected), actual, expected, descri
 );
 
 select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
- 'BWCR03' as step
- ,(select iff(count(*)=0, 0, count(*)/count(*))
-      from table(information_schema.query_history())
-      where query_text like 'SELECT snowflake.cortex.complete(\'mixtral%') as actual
- , 1 as expected
- ,'TRANSLATE REVIEWS!' as description
-);
-
-select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
  'BWCR04' as step
  ,(select iff(count(*)=0, 0, count(*)/count(*))
       from table(information_schema.query_history())
       where query_text like '%\'awful\') THEN \'awful\'%') as actual
  , 1 as expected
  ,'Sentiment!' as description
-);
-
-select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
- 'BWCR05' as step
- ,(select iff(count(*)=0, 0, count(*)/count(*))
-      from table(information_schema.query_history())
-      where query_text like '%[INST]### Write me survey report email%') as actual
- , 1 as expected
- ,'Write the email!' as description
 );
 
 -- Lab2: A Getting Started Guide With Snowflake Arctic and Snowflake Cortex
