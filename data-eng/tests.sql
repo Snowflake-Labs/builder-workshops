fa4332648cefbd54d6efc819b8c55791eb6f04f3
Gilberto Hernandez <gilberto.hernandez@snowflake.com>
2025-03-31 14:23:14 -0500

    Update tests.sql

--- data-eng/tests.sql
+++ data-eng/tests.sql
@@ -1,43 +1,40 @@
-use role accountadmin;
-use database util_db;
-use schema public;
+USE ROLE accountadmin;
+USE WAREHOUSE compute_wh;
+USE DATABASE tasty_bytes;
 
 select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
- 'BWDE3' as step
- ,( select count(*) from hol_db.information_schema.stages 
-   where stage_name = 'FROSTBYTE_RAW_STAGE') as actual
+ 'BWITD01' as step
+ ,(SELECT COUNT(*) FROM INFORMATION_SCHEMA.DATABASES WHERE DATABASE_NAME = 'tasty_bytes') as actual
  , 1 as expected
- ,'HOL_DB and FROSTBYTE_RAW_STAGE SUCCESSFULLY CREATED!' as description
+ ,'tasty_bytes database successfully created!' as description
 );
 
 select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
- 'BWDE4' as step
- ,( select count(*) from frostbyte_weathersource.information_schema.databases 
-   where database_name = 'FROSTBYTE_WEATHERSOURCE') as actual
+ 'BWITD02' as step
+ ,(SELECT COUNT(*) FROM tasty_bytes.raw_pos.country) as actual
+ , 30 as expected
+ ,'Data successfully copied into the country table!' as description
+);
+
+select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results 
+from (
+    SELECT
+        'BWITD03' as step,
+        (SELECT COUNT(*) FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME = 'hamburg_wind_speed_v' AND TABLE_SCHEMA = 'harmonized' AND TABLE_CATALOG = 'tasty_bytes') as actual, 1 as expected,         'hamburg_wind_speed_v view successfully created and contains correct data!' as description
+);
+
+select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results 
+from (
+    SELECT
+        'BWITD04' as step,
+        (SELECT COUNT(*) FROM INFORMATION_SCHEMA.FUNCTIONS WHERE FUNCTION_NAME IN ('FAHRENHEIT_TO_CELSIUS', 'INCH_TO_MILLIMETER') AND FUNCTION_SCHEMA = 'ANALYTICS' AND FUNCTION_CATALOG = 'TASTY_BYTES') as actual, 2 as expected, 'fahrenheit_to_celsius and inch_to_millimeter UDFs successfully created!' as description
+);
+
+select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
+ 'BWITD05' as step
+ ,(SELECT COUNT(*) FROM INFORMATION_SCHEMA.STREAMLITS WHERE STREAMLIT_TITLE = 'HAMBURG_GERMANY_TRENDS' AND STREAMLIT_CATALOG = 'TASTY_BYTES' AND STREAMLIT_SCHEMA = 'HARMONIZED') as actual
  , 1 as expected
- ,'FROSTBYTE_WEATHERSOURCE successfully loaded!' as description
+ ,'HAMBURG_GERMANY_TRENDS Streamlit app created and run successfully!' as description
 );
 
-select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
- 'BWDE5' as step
- ,( select count(*) from hol_db.information_schema.tables where table_name in ('LOCATION', 'ORDER_DETAIL')) as actual
- , 2 as expected
- ,'LOCATION AND ORDER_DETAIL tables successfully created.' as description
-);
-
-select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
- 'BWDE6' as step
- ,( select count(*) from hol_db.information_schema.procedures
-   where procedure_name = 'LOAD_DAILY_CITY_METRICS_SP') as actual
- , 1 as expected
- ,'LOAD_DAILY_CITY_METRICS_SP stored procedure successfully created.' as description
-);
-
-select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
- 'BWDE7' as step
- ,( select count(*) from hol_db.information_schema.object_privileges where object_name ilike '%HOL_DAG%') as actual
- , 4 as expected
- ,'DAG and tasks successfully created.' as description
-);
-
-SELECT 'You\'ve successfully completed this lab!' as STATUS;
+SELECT 'Congratulations! You have successfully completed the Snowflake Northstar - Data Engineering workshop!' as STATUS;
